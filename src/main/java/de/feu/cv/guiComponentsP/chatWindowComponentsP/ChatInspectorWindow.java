565e07ad858ed5a95cf1ab789885e02019dbba73
alejandro.casco.fernandez@gmail.com <alejandro.casco.fernandez@gmail.com@8b932a38-5663-ac3b-55d6-00b33135c56a>
2013-10-22 17:47:26 +0000

    Improved ChatInspectorWindow

--- src/main/java/de/feu/cv/guiComponentsP/chatWindowComponentsP/ChatInspectorWindow.java
+++ src/main/java/de/feu/cv/guiComponentsP/chatWindowComponentsP/ChatInspectorWindow.java
@@ -7,9 +7,11 @@ import de.feu.cv.applicationLogicP.conversationP.ThreadedMessage;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.*;
 
-public class ChatInspectorWindow {
+public class ChatInspectorWindow implements Observer {
 
     private int inspectorIndex = -1;
 
@@ -20,11 +22,15 @@ public class ChatInspectorWindow {
     private final JButton previousButton;
     private final JLabel indexLabel;
     private final JLabel sizeLabel;
+    private ChatListModel listModel;
 
 
     public ChatInspectorWindow(Conversation conversation) {
 
         this.conversation = conversation;
+        listModel = conversation.getBackgroundListModel();
+
+        conversation.addObserver(this);
 
         //Create and set up the window.
         frame = new JFrame("Conversation Inspector");
@@ -65,55 +71,55 @@ public class ChatInspectorWindow {
         frame.getContentPane().add(panelBotones, BorderLayout.SOUTH);
 
 
+
+
     }
 
-    private void previous() {
 
-        ChatListModel listModel = conversation.getBackgroundListModel();
+    private void showInspectionText() {
 
-        if (0 <= inspectorIndex && inspectorIndex < listModel.getSize()) {
-            inspectorIndex = Math.max(inspectorIndex - 1, 0);
-            showInspectionText(listModel);
-
-        }
-
-
-        //To change body of created methods use File | Settings | File Templates.
-    }
-
-    private void showInspectionText(ChatListModel listModel) {
-
-        ThreadedMessage message = (ThreadedMessage) listModel.get(inspectorIndex);
-        String text = "From: " + message.getOriginalJabberMessaje().getFrom() + "\r\n" +
-               "To: " + message.getOriginalJabberMessaje().getTo() + "\r\n" +
-                message.getText() + "\r\n";
-        if (message.getOriginalJabberMessaje() != null) {
-            java.util.Iterator<String> it = message.getOriginalJabberMessaje().getPropertyNames();
-            while (it.hasNext()) {
-                String name = it.next();
-                String value = (String) message.getOriginalJabberMessaje().getProperty(name);
-                text = text + "\r\n" + name + ": " + value;
+        if ((0 <= inspectorIndex) && (inspectorIndex < listModel.getSize())) {
+            ThreadedMessage message = (ThreadedMessage) listModel.get(inspectorIndex);
+            String text = "From: " + message.getOriginalJabberMessaje().getFrom() + "\r\n" +
+                    "To: " + message.getOriginalJabberMessaje().getTo() + "\r\n" +
+                    message.getText() + "\r\n";
+            if (message.getOriginalJabberMessaje() != null) {
+                java.util.Iterator<String> it = message.getOriginalJabberMessaje().getPropertyNames();
+                while (it.hasNext()) {
+                    String name = it.next();
+                    String value = (String) message.getOriginalJabberMessaje().getProperty(name);
+                    text = text + "\r\n" + name + ": " + value;
+                }
             }
+            inspectorTextLabel.setText(text);
         }
-        inspectorTextLabel.setText(text);
         indexLabel.setText("" + (inspectorIndex + 1));
         sizeLabel.setText(" de " + listModel.getSize());
     }
 
     private void next() {
-        ChatListModel listModel = conversation.getBackgroundListModel();
-
         if (inspectorIndex < listModel.getSize()) {
             inspectorIndex = Math.min(inspectorIndex + 1, listModel.getSize() - 1);
-            showInspectionText(listModel);
+            showInspectionText();
+        }
+    }
 
+    private void previous() {
+        if (0 <= inspectorIndex && inspectorIndex < listModel.getSize()) {
+            inspectorIndex = Math.max(inspectorIndex - 1, 0);
+            showInspectionText();
         }
     }
 
     public void open() {
         frame.pack();
         frame.setVisible(true);
+        showInspectionText();
     }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        showInspectionText();
+    }
 }
 
